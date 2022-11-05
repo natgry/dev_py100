3c270bc77f05371af10d6ef698f807c256f170de
natgry <natgry@yandex.ru>
2022-11-05 13:56:33 +0300

    Update task5.py

--- Лабораторная работа. Тема 5/task5.py
+++ Лабораторная работа. Тема 5/task5.py
@@ -1,11 +1,10 @@
-import string
+from string import ascii_uppercase, ascii_lowercase, digits
 from random import sample
 
 
 # TODO написать функцию генерации случайных паролей
 def get_random_password(n: int = 8) -> str:
-    chars = [string.ascii_uppercase, string.ascii_lowercase, string.digits]
-    password = sample("".join(chars), k=n)
+    password = sample(ascii_uppercase + ascii_lowercase + digits, k=n)
     return "".join(password)
 
 
